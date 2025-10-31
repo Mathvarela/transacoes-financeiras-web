9bc6d77ee2064c6e64038b95cfa2bd4d52957279
Matheus Varela <math.varela1622@gmail.com>
2025-10-31 13:53:45 -0300

    modificando para loadComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
-import { ListarTransacoesComponent } from '../components/transacoes/listar/listar_transacoes.component';
-import { CriarTransacoesComponent } from '../components/transacoes/criar/criar_transacoes.component';
-import { EditarTransacoesComponent } from '../components/transacoes/editar/editar_transacoes.component';
 
 export const routes: Routes = [
-    { path: '', 'redirectTo': '/listar-transacoes', pathMatch: 'full' },
-    { path: 'listar-transacoes', component: ListarTransacoesComponent },
-    { path: 'adicionar-transacoes', component: CriarTransacoesComponent },
-    { path: 'editar-transacoes/:id', component: EditarTransacoesComponent },
+    { path: '', redirectTo: '/listar-transacoes', pathMatch: 'full' },
+    {
+        path: 'listar-transacoes',
+        loadComponent: () => import('../components/transacoes/listar/listar_transacoes.component').then(m => m.ListarTransacoesComponent)
+    },
+    {
+        path: 'adicionar-transacoes',
+        loadComponent: () => import('../components/transacoes/criar/criar_transacoes.component').then(m => m.CriarTransacoesComponent)
+    },
+    {
+        path: 'editar-transacoes/:id',
+        loadComponent: () => import('../components/transacoes/editar/editar_transacoes.component').then(m => m.EditarTransacoesComponent)
+    },
 ];
